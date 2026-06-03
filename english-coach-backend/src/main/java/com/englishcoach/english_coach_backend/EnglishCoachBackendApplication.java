34bad2c0e1e53154faffaccd1a6fead440fd6ca7
NguyenHoaiThuong26 <21130559@st.hcmuaf.edu.vn>
2026-06-03 21:21:44 +0700

    Initial commit

---
+++ english-coach-backend/src/main/java/com/englishcoach/english_coach_backend/EnglishCoachBackendApplication.java
@@ -0,0 +1,13 @@
+package com.englishcoach.english_coach_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishCoachBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishCoachBackendApplication.class, args);
+	}
+
+}
---
+++ english-coach-backend/src/test/java/com/englishcoach/english_coach_backend/EnglishCoachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.englishcoach.english_coach_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishCoachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
